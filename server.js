47010c4c8d01fd5de5212f553f923a36af2da556
Shane Reustle <sreustle@gmail.com>
2012-06-08 00:24:32 -0400

    First cut of a basic logger with NO SAFETY FEATURES

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
 'use strict';
 
 var http = require('http');
+var url = require('url');
 var async = require('async');
 var mongo = require('mongojs');
 
-var db_simple_monitor = mongo.connect('127.0.0.1:27017/simple-monitor');
-var db_machines = db_simple_monitor.collection('machines');
-var db_machine_stats = db_simple_monitor.collection('machine_stats');
+var simple_logger_conn = mongo.connect('127.0.0.1:27017/simple-logger');
+var messages_db = simple_logger_conn.collection('messages');
 
 http.createServer(function(req, res){
-	
-	if(req.url == '/favicon.ico'){
-		var empty_gif = new Buffer(35);
-		empty_gif.write('R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=', 'base64');
-		res.send(empty_gif, { 'Content-Type' : 'image/gif' }, 200);
-		res.end('\n');
+	/*
+		Message Examples
 		
-		return;
-	}
+		?msg_type=js_error&err_file=app.js&err_line=123&err_msg=report+undefined
+		?msg_type=js_msg&msg=hello+world
+	*/
 	
 	console.log('[' + (new Date()) + '] ' + req.url);
 	
-	var friendly_url = req.url.split('/');
+	// Gather helpful information
+	var parsed_qs = url.parse(req.url, true).query;
+	var user_agent = req.headers['user-agent'];
 	
-	res.writeHead(200, {'Content-Type' : 'application/javascript'});
-		
-	db_machines.find().sort({name:1}, function(err, machines){
-		if(err){ throw err }
-			
-			
+	// TODO Use this to strip bad parsed_qs keys
+	var clean_qs_keys = ['err_msg', 'err_file', 'err_line', 'msg', 'msg_type'];
+	
+	// Fix known field types
+	if(parsed_qs.err_line){
+		parsed_qs.err_line = parseInt(parsed_qs.err_line, 10);
 	}
+	
+	// Create the document to insert
+	var insert_doc = parsed_qs;
+	insert_doc.time = parsed_qs.time || new Date();
+	insert_doc.user_agent = user_agent;
+	
+	// Insert the document
+	messages_db.insert(insert_doc);
+	
+	// Return an empty gif
+	var empty_gif = new Buffer(35);
+	empty_gif.write('R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=', 'base64');
+	res.writeHead(200, {'Content-Type' : 'image/gif'});
+	res.end(empty_gif);
 	
 }).listen(6543, '127.0.0.1');
 
